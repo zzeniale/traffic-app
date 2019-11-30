b15361123eee63656d3dfe4478d1e4d57d623330
Elaine Z <elainezw92@gmail.com>
2019-11-30 20:59:07 +0800

    u

--- app.py
+++ app.py
@@ -1,9 +1,23 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
-
+import os
 
 # initalise app
 app = Flask(__name__)
+
+# configure database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://syabijfoqfvajg:e6a840c9052355747fc5846aeb7c30496e66e9791da2d34344cab27a088d52ea@ec2-23-21-70-39.compute-1.amazonaws.com:5432/dd9l90skhvq2c0'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQLAlchemy(app)
+
+
+ # load data from Heroku PostgreSQL
+db_url = os.environ['postgres://syabijfoqfvajg:e6a840c9052355747fc5846aeb7c30496e66e9791da2d34344cab27a088d52ea@ec2-23-21-70-39.compute-1.amazonaws.com:5432/dd9l90skhvq2c0']
+conn = psycopg2.connect(db_url, sslmode='require')
+query = "SELECT time FROM {}".format(settings.traffic_images)
+df = pd.read_sql(query, con=conn)
+
 
 
 # create route
@@ -11,7 +25,7 @@ app = Flask(__name__)
 def index():
 
 
-    return "test"
+    return df.head()
     # render the template in link
     # return render_template('index.html', output = "testest")
 
