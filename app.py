0a98ecd550c873980aaf4bdc76a174d9a0cd0b11
Elaine Z <elainezw92@gmail.com>
2019-11-30 20:49:22 +0800

    u

--- app.py
+++ app.py
@@ -1,27 +1,21 @@
 from flask import Flask, render_template, request, jsonify
-import numpy as np 
-import os
-from PIL import Image
-# from flask_apscheduler import APScheduler
+from flask_sqlalchemy import SQLAlchemy
+
 
 # initalise app
 app = Flask(__name__)
-
-# import (cropped) images
-img_files = [f for f in os.listdir("./images") if f != '.DS_Store']
-img_files = img_files[-6:]
-
-# background subtraction
 
 
 # create route
 @app.route('/') # homepage
 def index():
+
+
+    return "test"
     # render the template in link
-    return render_template('index.html', output = "testest")
+    # return render_template('index.html', output = "testest")
 
 
 if __name__ == '__main__':
-    # app.debug = True
     # run the app
     app.run()
