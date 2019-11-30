8a0d2b2765c40c95d010ea33bb2742cb5494ca59
Elaine Z <elainezw92@gmail.com>
2019-11-30 21:33:55 +0800

    u

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ app = Flask(__name__)
 # connect to Heroku PostgreSQL
 db_url = 'postgres://syabijfoqfvajg:e6a840c9052355747fc5846aeb7c30496e66e9791da2d34344cab27a088d52ea@ec2-23-21-70-39.compute-1.amazonaws.com:5432/dd9l90skhvq2c0'
 app.config['SQLALCHEMY_DATABASE_URI'] = db_url
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 engine = db.create_engine(db_url, {})
 df = pd.read_sql_query("SELECT image FROM traffic_images ORDER BY db_id DESC LIMIT 6", engine)
@@ -17,8 +18,8 @@ df = pd.read_sql_query("SELECT image FROM traffic_images ORDER BY db_id DESC LIM
 # create route
 @app.route('/') # homepage
 def index():
-    plt.imshow()
-    return df.loc[0,'image']
+    sample = df.loc[0,'image']
+    return plt.imshow(sample)
     # render the template in link
     # return render_template('index.html', output = "testest")
 
