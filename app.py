361c519b6930bcb4fd0aef536e60cb223a289923
Elaine Z <elainezw92@gmail.com>
2019-11-30 21:36:42 +0800

    u

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ df = pd.read_sql_query("SELECT image FROM traffic_images ORDER BY db_id DESC LIM
 @app.route('/') # homepage
 def index():
     sample = df.loc[0,'image']
-    return plt.imshow(sample)
+    return sample.shape[0]
     # render the template in link
     # return render_template('index.html', output = "testest")
 
