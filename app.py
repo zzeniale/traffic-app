295ee35aa1c74b93f594e9fcba09784c00a9c065
Elaine Z <elainezw92@gmail.com>
2019-12-02 14:43:26 +0800

    u

--- app.py
+++ app.py
@@ -15,11 +15,47 @@ db = SQLAlchemy(app)
 engine = db.create_engine(db_url, {})
 images = pd.read_sql_query("SELECT image FROM traffic_images ORDER BY db_id DESC LIMIT 6", engine)
 
+# process images
+processed_images = []
+bg_sub = cv.createBackgroundSubtractorMOG2(history = 5, varThreshold=100, detectShadows=False)
+for image in images.values:
+    image = np.array(image[0])
+    # crop image
+    image = image[60:320, 380:]
+    image[:185, 240:] = 0
+    image[:, 205:] = 0
+    image[:170, 200:] = 0
+    image[:165, 180:] = 0
+    image[:158, 160:] = 0
+    image[:153, 150:] = 0
+    image[:143, 144:] = 0
+    image[50:135, 130:] = 0
+    image[60:130, 120:] = 0
+    image[70:125, 110:] = 0
+    image[80:115, 100:] = 0
+    image[90:110, 95:] = 0
+    for length, width in zip(np.arange(5,96,5), np.arange(150,0,-10)):
+        image[:length,:width]=0
+    for length, width in zip(np.arange(150,245, 5), np.arange(0,190,10)):
+        image[length:,:width]=0
+    # subtract background
+    fg = bg_sub.apply(image,learningRate=0.7)
+    denoise_tv = denoise_tv_chambolle(fg, weight=0.15, multichannel=False)
+    ret,thresh = cv.threshold(denoise_tv,0.15,255,cv.THRESH_BINARY)
+    fill = ndi.binary_fill_holes(thresh)
+    fill = img_as_ubyte(fill)
+    processed_images.append(fill)
+
+# import model
+from xgboost import XGBRegressor
+import joblib
+model = joblib.load('xgb.model')
+prediction = model.predict(processed_images[-1])
+
 # create route
 @app.route('/') # homepage
 def index():
-    sample = df.loc[0,'image']
-    return f"{len(sample)}"
+    return prediction
     
     # render the template in link
     # return render_template('index.html', output = "testest")
