94fcc05ba77ba6a8aa4e7eda04e2d068219d456c
Elaine Z <elainezw92@gmail.com>
2019-12-03 10:21:52 +0800

    u

--- app.py
+++ app.py
@@ -91,10 +91,9 @@ prediction = model.predict(foreground_px.loc[5:,:])
 @app.route('/') # homepage
 def index():
     value = int(prediction[0])
-    return f"{value}"
+    # return f"{value}"
     
-    # render the template in link
-    # return render_template('index.html', output = "testest")
+    return render_template('index.html', output = value)
 
 
 if __name__ == '__main__':
